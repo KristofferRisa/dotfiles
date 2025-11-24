59d2a9f8dc79e3577e6ae39b5ba5db17597925f7
Kristoffer Risa <kristofferrisa@gmail.com>
2025-11-24 23:26:15 +0100

    updated nvim config

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,33 +1,33 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons", -- for file icons
-	},
-	opts = {
-		filters = {
-			dotfiles = false, -- Show hidden files/folders
-			git_ignored = false, -- Show git-ignored files
-			-- custom = { "^.git$" }, -- Hide .git folder specifically
-		},
-		view = {
-			width = 30,
-			side = "left",
-		},
-		renderer = {
-			icons = {
-				show = {
-					file = true,
-					folder = true,
-					folder_arrow = true,
-					git = true,
-					hidden = true,
-				},
-			},
-		},
-		actions = {
-			open_file = {
-				quit_on_open = false,
-			},
-		},
-	},
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- for file icons
+  },
+  opts = {
+    filters = {
+      dotfiles = true, -- Show hidden files/folders
+      git_ignored = true, -- Show git-ignored files
+      -- custom = { "^.git$" }, -- Hide .git folder specifically
+    },
+    view = {
+      width = 30,
+      side = "left",
+    },
+    renderer = {
+      icons = {
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = true,
+          hidden = true,
+        },
+      },
+    },
+    actions = {
+      open_file = {
+        quit_on_open = false,
+      },
+    },
+  },
 }
